07ddc3aef2fe92286abb1cf8b869303a15c05663
Aashish Pandey <aashishkpandey@gmail.com>
2017-08-22 18:42:27 +0530

    Initial App

---
+++ dependency-text-segmentation-shinyapp.R
@@ -0,0 +1,27 @@
+require(shiny) || install.packages("shiny")
+require(text2vec) || install.packages("text2vec")
+require(tm) || install.packages("tm")
+require(tokenizers) || install.packages("tokenizers")
+require(wordcloud) || install.packages("wordcloud")
+require(slam) || install.packages("slam")
+
+require(stringi) || install.packages("stringi")
+require(magrittr) || install.packages("magrittr")
+require(tidytext) || install.packages("tidytext")
+require(dplyr) || install.packages("dplyr")
+require(tidyr) || install.packages("tidyr")
+
+
+library(shiny)
+library(text2vec)
+library(tm)
+library(tokenizers)
+library(wordcloud)
+library(slam)
+# Update test 123
+
+library(stringi)
+library(magrittr)
+library(tidytext)
+library(dplyr)
+library(tidyr)
